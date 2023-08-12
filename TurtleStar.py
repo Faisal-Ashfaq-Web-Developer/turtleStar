2da1aee410e69dd2fb90cc81f6fd67d8d11cd77a
Faisal Ashfaq <faisal0ashfaq@gmail.com>
2023-08-13 03:16:56 +0530

    Add files via upload

---
+++ TurtleStar.py
@@ -0,0 +1,13 @@
+import turtle as t
+
+t.bgcolor("maroon")
+t.pencolor("yellow")
+t.pensize(5)
+t.up()
+t.goto(-200,50)
+t.down()
+for i in range(5):
+    t.forward(400)
+    t.right(144)
+t.hideturtle()
+t.done()
